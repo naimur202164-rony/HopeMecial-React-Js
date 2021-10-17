1d2d905d2e2ccbe0dcbfa4ac5b6b64334e17172b
unknown <you@example.com>
2021-10-18 04:32:30 +0600

    dd

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDin41fuxa0VNHG6457xAacH6x00UW_N-Y",
+    authDomain: "assinmnet10.firebaseapp.com",
+    projectId: "assinmnet10",
+    storageBucket: "assinmnet10.appspot.com",
+    messagingSenderId: "978874443812",
+    appId: "1:978874443812:web:f100d27bf4a5b736c690b1"
+  };
